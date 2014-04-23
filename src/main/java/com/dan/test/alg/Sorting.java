358f2f21833fa7f8914105ac983fd5e067bfc9ec
Dan Borza <dan.borza@intel.com>
2014-04-23 18:42:27 +0300

    MergeSort. Small bug wip

--- src/main/java/com/dan/test/alg/Sorting.java
+++ src/main/java/com/dan/test/alg/Sorting.java
@@ -26,7 +26,7 @@ public class Sorting {
 
         final int [] input = { 1, 3, -3, 10, 11, 10, -100, 14, 100, 34 };
 
-        final Sorter sorter = new SelectionSorter();
+        final Sorter sorter = new MergeSort();
 
         System.out.println(Arrays.toString(sorter.sort(input)));
     }
@@ -53,6 +53,78 @@ public class Sorting {
             }
 
             return input;
+        }
+
+    }
+
+    private static class MergeSort implements Sorter {
+
+        public int[] sort(int[] input) {
+
+            if (input == null) {
+                throw new IllegalStateException("Input array is null. Cannot merge sort.");
+            }
+
+            if (input.length == 1) {
+                return new int [] { input [0] };
+            }
+
+            final int middle = input.length / 2;
+
+            int [] left = copyOf(input, 0, middle - 1);
+            int [] right = copyOf(input, middle, input.length - 1);
+
+            return merge(sort(left), sort(right));
+        }
+
+        /**
+         * Given two sorted input arrays, merged them into a resulting sorted array.
+         * @param a
+         *          First sorted input array.
+         * @param b
+         *          Second sorted input array.
+         * @return
+         *          Resulting sorting array.
+         */
+        private static int [] merge (int [] a, int [] b) {
+
+            if (a == null || b == null) {
+                throw new IllegalStateException("One of the input arrays is null. Cannot merge sort.");
+            }
+
+            int [] result = new int [a.length + b.length];
+
+            int ib = 0;
+            int ir = 0;
+
+            for (int ia = 0; ia < a.length; ia ++) {
+                while (b[ib] <= a[ia]) {
+                    result[ir] = b[ib];
+                    ir ++;
+                    ib ++;
+                }
+                result[ir] = a[ia];
+                ir ++;
+                ia ++;
+            }
+
+            while (ib < b.length) {
+                result[ir] = b[ib];
+                ir ++;
+                ib ++;
+            }
+
+            return result;
+        }
+
+        public int [] copyOf(int [] input, int start, int stop) {
+            int [] result = new int [stop - start + 1];
+
+            for (int ir = 0; ir < result.length; ir ++) {
+                result[ir] = input[start + ir];
+            }
+
+            return result;
         }
 
     }
