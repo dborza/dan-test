745e2afc7f0d9c92f5a8e01312205a94620005e4
Dan Borza <dan.borza@intel.com>
2014-04-26 18:44:25 +0300

    BFS

--- src/main/java/com/dan/test/alg/GraphAdjacencyList.java
+++ src/main/java/com/dan/test/alg/GraphAdjacencyList.java
@@ -10,18 +10,25 @@ import java.util.*;
 public class GraphAdjacencyList {
 
     /**
-     * A noVertexde in the graph.
+     * A vertex in the graph.
      */
     static class Vertex {
 
-        public Vertex(int v) {
-            this.v = v;
+        public Vertex(int id) {
+            this.id = id;
         }
 
         /**
-         * Value of the node (Usually not needed. Can be used as an id)
+         * Id of the node. Should be unique. Won't check for uniqueness for now.
          */
-        int v;
+        int id;
+
+        @Override
+        public String toString() {
+            return "Vertex{" +
+                    "id=" + id +
+                    '}';
+        }
     }
 
     /**
@@ -68,6 +75,46 @@ public class GraphAdjacencyList {
         }
     }
 
+    /**
+     * Do a Breadth First Search on a {@link Graph} starting with a given {@link Vertex}.
+     *
+     * @return a {@code List} of {@link Vertex}es processed in the order of bfs.
+     */
+    static List<Vertex> bfs(Graph g, Vertex start) {
+
+        if (g == null) {
+            throw new IllegalArgumentException("Input graph must not be null.");
+        }
+
+        final List<Vertex> returnList = new LinkedList<Vertex>();
+
+        final Queue<Vertex> toVisit = new LinkedList<Vertex>();
+        toVisit.add(start);
+
+        while (!toVisit.isEmpty()) {
+
+            final Vertex currentVertex = toVisit.remove();
+
+            returnList.add(currentVertex);
+
+            final List<Edge> edges = g.adjacencyList.get(currentVertex);
+
+            if (edges != null) {
+                for (final Edge e : edges) {
+                    if (!returnList.contains(e.y) && !toVisit.contains(e.y)) {
+                        toVisit.add(e.y);
+                    }
+                }
+            }
+        }
+
+        //  Start with a node
+        //  Iterate through all it's neighbours
+        //  Do the thing you want to do on them and put them in the queue
+
+        return returnList;
+    }
+
     public static void main(String[] args) {
 
         Vertex v1 = new Vertex(1);
@@ -77,12 +124,14 @@ public class GraphAdjacencyList {
         Vertex v5 = new Vertex(5);
 
         Edge e1 = new Edge(v1, v2, 1);
-        Edge e2 = new Edge(v2, v3, 2);
-        Edge e3 = new Edge(v3, v4, 3);
-        Edge e4 = new Edge(v4, v5, 4);
+        Edge e2 = new Edge(v1, v5, 2);
+        Edge e3 = new Edge(v5, v4, 3);
+        Edge e4 = new Edge(v4, v3, 4);
         Edge e5 = new Edge(v5, v1, 5);
 
         Graph g = new Graph(e1, e2, e3, e4, e5);
+
+        System.out.println(bfs(g, v1));
     }
 
 
